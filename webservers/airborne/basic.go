3c06a73bd8ae03e67d8f1ecebb2d5831659b39ab
Gustavo <gustavo.sainz@gmail.com>
2022-02-19 11:17:49 -0500

    Update basic.go

--- webservers/airborne/basic.go
+++ webservers/airborne/basic.go
@@ -11,7 +11,7 @@ func setupServer() *airborne.Mux {
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("Chi web server running..."))
+		_, _ = w.Write([]byte("Airborne router running..."))
 	})
 
 	return r
